46aba9bbb25ff524a7b983648bd08a707177c466
Adam Gerber <adam.gerber@yahoo.com>
2013-09-18 20:03:58 -0500

    more methods of Boxer

--- src/P0_2/Boxer.java
+++ src/P0_2/Boxer.java
@@ -1,5 +1,7 @@
 package P0_2;
 
+import java.util.Random;
+
 /**
  * Created with IntelliJ IDEA.
  * User: ag
@@ -9,21 +11,43 @@ package P0_2;
  */
 public class Boxer {
 
+
     private static final int MAX_HIT_POINTS = 100;
     private static final String[] EXCLAMS = {"I'm the greatest", "Float like a butterfly, sting like a bee", "Victory is mine", "Haahaahahaha, I win.", "Adrian!"};
     //Each boxer has the following properties; accuracy, strength, hit-points, name, exclamation.
     private double mAccuracy;   //percentage
-    private int mStrength;      //how strong is the fighter, as in much does it sting when he connects
+    private int mStrength;      //how strong is the fighter, as in much does it sting when he connects  1-5
     private int mHitPoints;     //initialize to 100
     private String mName;  //fighters name
+    private Random mRandom;
 
 
     public Boxer(String name, int strength, double accuracy) {
         mName = name;
         mStrength = strength;
         mAccuracy = accuracy;
+        mRandom = new Random();
     }
 
+
+    public void swing(Boxer boxPunchee){
+
+      if (mRandom.nextInt(10) * mAccuracy > 3){
+          boxPunchee.setHitPoints(boxPunchee.getHitPoints() - mStrength);
+      }
+
+    }
+
+    public boolean isKO(){
+        return mHitPoints <= 0;
+    }
+
+    public String win(){
+        return mName + " wins: " + EXCLAMS[mRandom.nextInt(EXCLAMS.length)];
+    }
+
+
+    //getters and setters
     public String getName() {
         return mName;
     }
