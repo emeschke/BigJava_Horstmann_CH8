17853b787def0cafbf02a49a19bdd9212515ff93
Adam Gerber <adam.gerber@yahoo.com>
2013-09-18 19:13:55 -0500

    initial commit with instructions

---
+++ src/P0_2/Boxer.java
@@ -0,0 +1,12 @@
+package P0_2;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ag
+ * Date: 9/18/13
+ * Time: 7:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Boxer {
+
+}
---
+++ src/P0_2/Driver.java
@@ -0,0 +1,32 @@
+package P0_2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ Name your packages according to its Programming Exercise identifier, except replace the period with underscore.
+ For example, if the Programming Exercise identifier is P1.15, then name your package file P1_15
+ */
+public class Driver {
+
+    public static void main(String[] args) {
+
+        //Example: Create a program that pits two boxers against one another in an ultimate match to KO.
+        //Each boxer has 100 hit points. When a boxer connects his punch, he extracts hit-points from his opponent
+        //Each boxer has the following properties; accuracy, strength, hit-points, name, exclamation.
+
+        //pseudocode
+        //Create two boxers
+        //While both still standing
+            //pucher swings at punchee
+            //swap puncher and punchee
+        //victor exclaims victory
+
+
+
+
+    }
+
+
+}
