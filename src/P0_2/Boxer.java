f0a5c8d560086028580d91950a8200cb51a54906
Adam Gerber <adam.gerber@yahoo.com>
2013-09-18 19:49:04 -0500

    Boxer's fields

--- src/P0_2/Boxer.java
+++ src/P0_2/Boxer.java
@@ -9,4 +9,20 @@ package P0_2;
  */
 public class Boxer {
 
+    private static final int MAX_HIT_POINTS = 100;
+    private static final String[] EXCLAMS = {"I'm the greatest", "Float like a butterfly, sting like a bee", "Victory is mine", "Haahaahahaha, I win.", "Adrian!"};
+    //Each boxer has the following properties; accuracy, strength, hit-points, name, exclamation.
+    private double mAccuracy;   //percentage
+    private int mStrength;      //how strong is the fighter, as in much does it sting when he connects
+    private int mHitPoints;     //initialize to 100
+    private String mName;  //fighters name
+
+
+
+
+
+
+
+
+
 }
